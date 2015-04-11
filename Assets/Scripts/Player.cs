d30c7f8f1b3262f08c8532429f8bde3fdef85b21
Patryk Kochański <patryk.kochanski@hotmail.com>
2015-04-11 18:19:01 +0200

    Player template class

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
 
-	// Use this for initialization
+    #region Variables
+
+    
+    #endregion
+
+    #region Properties
+
+    #endregion
+
+    #region Methods
+
+    public void Move(ActionDirection dir)
+    {
+        
+    }
+
+    public void KindleFire()
+    {
+
+    }
+
+    public void SetTrap(ActionDirection dir)
+    {
+
+    }
+
+    public void ThrowSpear(Vector2 target)
+    {
+
+    }
+
+    // Use this for initialization
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
+    #endregion
+
+    #region EnumForActionDirection
+    public enum ActionDirection
+    {
+        Up = 1,
+        Right,
+        Down,
+        Left
+    }
+    #endregion
 }
