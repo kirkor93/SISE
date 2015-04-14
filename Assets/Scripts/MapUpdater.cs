fcddcc14ea0c784530252d60bee12ca2acd6292c
trolandus <johnnybe.beczcki@gmail.com>
2015-04-14 14:54:55 +0200

    Added methods for changing field type and removing field type (changing to none) on certain positions

--- Assets/Scripts/MapUpdater.cs
+++ Assets/Scripts/MapUpdater.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class MapUpdater : MonoBehaviour
 {
     public GameObject[] Players;
+    public QueueController Controller;
 
 	protected void Start () 
     {
-	
+
 	}
 	
 	protected void Update () 
@@ -15,5 +16,17 @@ public class MapUpdater : MonoBehaviour
 	    
 	}
 
+    public void ChangeFieldOnMap(Vector2 position, FieldType type)
+    {
+        position.x += 50; position.x /= 2;
+        position.y += 50; position.y /= 2;
+        Controller.GetMapState().MapArray[(int)position.x, (int)position.y] = type;
+    }
 
+    public void RemoveNonEmptyField(Vector2 position)
+    {
+        position.x += 50; position.x /= 2;
+        position.y += 50; position.y /= 2;
+        Controller.GetMapState().MapArray[(int)position.x, (int)position.y] = FieldType.EMPTY;
+    }
 }
