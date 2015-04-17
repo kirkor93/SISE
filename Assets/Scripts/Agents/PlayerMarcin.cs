1c4dc924eab443b6bb793705ff345d67d75e748c
Krzysztof Stengert <krzysztof.stengert@hotmail.com>
2015-04-17 08:42:04 +0200

    Added input for moving in every direction for three players

--- Assets/Scripts/Agents/PlayerMarcin.cs
+++ Assets/Scripts/Agents/PlayerMarcin.cs
@@ -11,5 +11,20 @@ public class PlayerMarcin : Bot
             Controller.Move(ActionDirection.Up);
             Controller.EndTurn();
         }
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Controller.Move(ActionDirection.Down);
+            Controller.EndTurn();
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Controller.Move(ActionDirection.Left);
+            Controller.EndTurn();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Controller.Move(ActionDirection.Right);
+            Controller.EndTurn();
+        }
     }
 }
