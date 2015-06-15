0a3a526207cd9aa40a24f2fcca98ec0b1b6f1208
trolandus <johnnybe.beczcki@gmail.com>
2015-06-15 14:17:44 +0200

    Bot is walking due to certain conditions.

--- src/MajsterBot.java
+++ src/MajsterBot.java
@@ -21,20 +21,39 @@ public class MajsterBot extends Bot
 		this.Clips.assertString("(bot (hitPoints " + Broker.GetMyHP() + ")" +
 								"(state current))");
 		System.out.println("Majster");
-//		if(Broker.GetMyHP() == 0)
-//		{
-//			this.Clips.eval("(facts)");
-//		}
-		
-		String evalStr = "(find-all-facts ((?f bot)) TRUE)";
+
+		String evalStr = "(find-all-facts ((?f bot)) (not (eq ?f:direction nil)))";
 		
 		MultifieldValue pv = (MultifieldValue) this.Clips.eval(evalStr);
 		int tNum = pv.listValue().size();
 		FactAddressValue fv = (FactAddressValue) pv.listValue().get(tNum - 1);
 		String currentHP = fv.getFactSlot("hitPoints").toString();
+		String currentDirection = fv.getFactSlot("direction").toString();
 		
-		System.out.println("Current HP: " + currentHP);
+		System.out.println("Current HP: " + Broker.GetMyHP());
+		if(Broker.GetMyHP() == 0)
+		{
+			System.out.println("Majster is dead");
+			return;
+		}
 		
-		this.Clips.reset();
+		System.out.println("Current direction: " + currentDirection);
+		System.out.println("Position: " + Broker.GetMyPosition().Y);
+		
+		switch(currentDirection)
+		{
+		case "up": 
+			Broker.Action(ActionType.MOVE, new Vector2(0, -1));
+			break;
+		case "down":
+			Broker.Action(ActionType.MOVE, new Vector2(0, 1));
+			break;
+		default:
+			Broker.Action(ActionType.MOVE, new Vector2(0, -1));
+			break;
+		}
+		
+		//this.Clips.reset();
+		this.Clips.run();
 	}
 }
--- src/MajsterClips.clp
+++ src/MajsterClips.clp
@@ -2,7 +2,20 @@
 	(slot hitPoints (default 15))
 	(slot actionPoints (default 5))
 	(slot woodPoints (default 0))
-	(slot state (default initial)))
+	(slot state (default initial))
+	(slot direction))
 	
 (deffacts startup
 	(bot))
+	
+(defrule goUp
+	(bot (hitPoints ?hp) (woodPoints ?wp))
+	(test (eq ?wp 0))
+	=>
+	(assert (bot (hitPoints ?hp) (direction up))))
+	
+(defrule goDown
+	(bot (hitPoints ?hp))
+	(test (< ?hp 5))
+	=>
+	(assert (bot (hitPoints ?hp) (direction down))))
